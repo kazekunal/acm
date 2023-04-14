f8909cd67b797deebd326b741d83427a84f80fc7
kunal passan <Kunalpassan30@gmail.com>
2023-04-15 01:41:39 +0530

    feat: body updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './component/navbar';
+import Home from './component/home'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/component/home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../styles/home.css'
+import background_image from '../assets/dashboard-image.png'
+
+function home() {
+  return (
+    <>
+    <div className='home_button'>
+        <p>
+            Just launched ghost 3.0
+        </p>
+    </div>
+    <div className='home_heading'>
+        <p>
+            Turn your audience <br/><span className='business'> into a  business </span> 
+        </p>
+    </div>
+    <div className='home_background'>
+
+    </div>
+    <div className='home_image'>
+        <img src={background_image} alt='img'/>
+    </div>
+    </>
+  )
+}
+
+export default home
